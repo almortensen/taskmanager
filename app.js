8dcdff77acfe2e1622d08c58047791c0621a68d0
Aiden Mortensen <aidenlm9120@gmail.com>
2025-05-24 20:57:48 -0400

    change app.js,index.html, and index.js

---
+++ app.js
@@ -0,0 +1,17 @@
+async function makeTask(catPOS) {
+    
+}
+
+async function startProgram(){
+    
+}
+
+
+
+async function LoadCat(catPOS){
+
+}
+
+async function getTask(catPOS, taskPOS) {
+    
+}
